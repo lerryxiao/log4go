44279f98147d8ecd2e128cc9c6675481d95f7772
zhoutao <zhout@hiscene.com>
2019-07-16 11:13:47 +0800

    update

--- log/define/logger.go
+++ log/define/logger.go
@@ -125,12 +125,7 @@ func (log Logger) Log(lvl uint8, source, message string) {
 }
 
 // Logf format 日志输出
-func (log Logger) Logf(lvl uint8, format string, args ...interface{}) {
-	log.intLogf(1, lvl, format, args...)
-}
-
-// Logfs format 日志输出
-func (log Logger) Logfs(skip int, lvl uint8, format string, args ...interface{}) {
+func (log Logger) Logf(skip int, lvl uint8, format string, args ...interface{}) {
 	if skip <= 0 {
 		skip = 1
 	}
@@ -155,121 +150,121 @@ func (log Logger) LogReport(skip int, rptp, extp uint8, exdt ...interface{}) {
 	log.dispatchLog(record, rptp)
 }
 
-func (log Logger) getArg(arg0 interface{}, larg int) string {
+func (log Logger) getArg(arg interface{}, larg int) string {
 	var msg string
-	switch first := arg0.(type) {
+	switch first := arg.(type) {
 	case string:
 		msg = first
 	case func() string:
 		msg = first()
 	default:
-		msg = fmt.Sprint(arg0) + strings.Repeat(" %v", larg)
+		msg = fmt.Sprint(arg) + strings.Repeat(" %v", larg)
 	}
 	return msg
 }
 
 // LogCmm 日志输出处理
-func (log Logger) LogCmm(lvl uint8, arg0 interface{}, args ...interface{}) {
-	log.Logf(lvl, log.getArg(arg0, len(args)), args...)
+func (log Logger) LogCmm(lvl uint8, arg interface{}, args ...interface{}) {
+	log.LogCmms(4, lvl, arg, args...)
 }
 
 // LogCmms 日志输出处理
-func (log Logger) LogCmms(skip int, lvl uint8, arg0 interface{}, args ...interface{}) {
-	log.Logfs(skip, lvl, log.getArg(arg0, len(args)), args...)
+func (log Logger) LogCmms(skip int, lvl uint8, arg interface{}, args ...interface{}) {
+	log.Logf(skip, lvl, log.getArg(arg, len(args)), args...)
 }
 
 // Finest 最好log
-func (log Logger) Finest(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(FINEST, arg0, args...)
+func (log Logger) Finest(arg interface{}, args ...interface{}) {
+	log.LogCmm(FINEST, arg, args...)
 }
 
 // Fine 好log
-func (log Logger) Fine(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(FINE, arg0, args...)
+func (log Logger) Fine(arg interface{}, args ...interface{}) {
+	log.LogCmm(FINE, arg, args...)
 }
 
 // Debug 调试log
-func (log Logger) Debug(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(DEBUG, arg0, args...)
+func (log Logger) Debug(arg interface{}, args ...interface{}) {
+	log.LogCmm(DEBUG, arg, args...)
 }
 
 // Trace 追踪log
-func (log Logger) Trace(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(TRACE, arg0, args...)
+func (log Logger) Trace(arg interface{}, args ...interface{}) {
+	log.LogCmm(TRACE, arg, args...)
 }
 
 // Info 信息log
-func (log Logger) Info(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(INFO, arg0, args...)
+func (log Logger) Info(arg interface{}, args ...interface{}) {
+	log.LogCmm(INFO, arg, args...)
 }
 
 // Warn 警告log
-func (log Logger) Warn(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(WARNING, arg0, args...)
+func (log Logger) Warn(arg interface{}, args ...interface{}) {
+	log.LogCmm(WARNING, arg, args...)
 }
 
 // Error 错误log
-func (log Logger) Error(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(ERROR, arg0, args...)
+func (log Logger) Error(arg interface{}, args ...interface{}) {
+	log.LogCmm(ERROR, arg, args...)
 }
 
 // Fatal 致命log
-func (log Logger) Fatal(arg0 interface{}, args ...interface{}) {
-	log.LogCmm(FATAL, arg0, args...)
+func (log Logger) Fatal(arg interface{}, args ...interface{}) {
+	log.LogCmm(FATAL, arg, args...)
 }
 
 // Report 上报log
-func (log Logger) Report(rptp uint8, arg0 interface{}, args ...interface{}) {
-	log.Reports(2, rptp, arg0, args...)
+func (log Logger) Report(rptp uint8, arg interface{}, args ...interface{}) {
+	log.Reports(4, rptp, arg, args...)
 }
 
 // Reports 上报log
-func (log Logger) Reports(skip int, rptp uint8, arg0 interface{}, args ...interface{}) {
-	msg := log.getArg(arg0, len(args))
+func (log Logger) Reports(skip int, rptp uint8, arg interface{}, args ...interface{}) {
+	msg := log.getArg(arg, len(args))
 	if len(args) > 0 {
 		msg = fmt.Sprintf(msg, args...)
 	}
 	if skip <= 0 {
-		skip = 1
+		skip = 2
 	}
 	log.LogReport(skip, rptp, 0, msg)
 }
 
 // Flume flume上报
-func (log Logger) Flume(arg0 interface{}, args ...interface{}) {
-	log.Report(FLUME, arg0, args...)
+func (log Logger) Flume(arg interface{}, args ...interface{}) {
+	log.Report(FLUME, arg, args...)
 }
 
 // FlumeAPI flume api上报
 func (log Logger) FlumeAPI(url string, header interface{}, body interface{}) {
-	log.LogReport(1, FLUME, EXUrlHeadBody, url, header, body)
+	log.LogReport(2, FLUME, EXUrlHeadBody, url, header, body)
 }
 
 // CatTransaction cat transaction支持
 func (log Logger) CatTransaction(name string, status interface{}, data interface{}) {
-	log.LogReport(1, CAT, EXCatTransaction, name, status, data)
+	log.LogReport(2, CAT, EXCatTransaction, name, status, data)
 }
 
 // CatEvent cat event支持
 func (log Logger) CatEvent(name string, status interface{}, data interface{}) {
-	log.LogReport(1, CAT, EXCatEvent, name, status, data)
+	log.LogReport(2, CAT, EXCatEvent, name, status, data)
 }
 
 // CatError cat error支持
 func (log Logger) CatError(name string, err interface{}) {
-	log.LogReport(1, CAT, EXCatError, name, err)
+	log.LogReport(2, CAT, EXCatError, name, err)
 }
 
 // CatMetricCount cat metric count支持
 func (log Logger) CatMetricCount(name string, count ...int) {
 	if len(count) <= 0 {
-		log.LogReport(1, CAT, EXCatMetricCount, name)
+		log.LogReport(2, CAT, EXCatMetricCount, name)
 	} else {
-		log.LogReport(1, CAT, EXCatMetricCount, name, count[0])
+		log.LogReport(2, CAT, EXCatMetricCount, name, count[0])
 	}
 }
 
 // CatMetricDuration cat metric duration支持
 func (log Logger) CatMetricDuration(name string, duration int64) {
-	log.LogReport(1, CAT, EXCatMetricDuration, name, duration)
+	log.LogReport(2, CAT, EXCatMetricDuration, name, duration)
 }
